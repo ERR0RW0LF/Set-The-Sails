510d74ef6b69e16c1b1f7f9a14f339ba94a94bbf
Ole Snyders <124588267+IBimsHedebe@users.noreply.github.com>
2025-12-30 00:17:12 +0100

    Implement Items

---
+++ src/main/java/com/ERR0RW0LF/set_the_sails/item/ModItems.java
@@ -0,0 +1,21 @@
+package com.ERR0RW0LF.set_the_sails.item;
+
+import com.ERR0RW0LF.set_the_sails.Set_the_sails;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item PLANK = registerItems("plank", new Item(new Item.Settings()));
+
+    private static Item registerItems(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(Set_the_sails.MOD_ID, name), item);
+    }
+
+    private static void registerModItems(){
+        Set_the_sails.LOGGER.info("Registering Mod Items for " + Set_the_sails.MOD_ID);
+    }
+
+}
