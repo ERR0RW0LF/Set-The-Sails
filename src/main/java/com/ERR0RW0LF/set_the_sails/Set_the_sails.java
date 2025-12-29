d8d4cf9c6322fb07ea262cf17dca746d8a4df9dc
ERR0RW0LF <finn.woma@gmail.com>
2025-12-29 23:59:28 +0100

    add logging functionality to mod initialization

--- src/main/java/com/ERR0RW0LF/set_the_sails/Set_the_sails.java
+++ src/main/java/com/ERR0RW0LF/set_the_sails/Set_the_sails.java
@@ -1,10 +1,16 @@
 package com.ERR0RW0LF.set_the_sails;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Set_the_sails implements ModInitializer {
+    public static final String MOD_ID = "set_the_sails";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 
     @Override
     public void onInitialize() {
+        LOGGER.info("Started Mod");
     }
 }
