39f03153a5f3b95551a48ddf95bfd8d115186057
Ole Snyders <124588267+IBimsHedebe@users.noreply.github.com>
2025-12-30 00:37:49 +0100

    Implement Items - added "Plank"

--- src/main/java/com/ERR0RW0LF/set_the_sails/client/Set_the_sailsClient.java
+++ src/main/java/com/ERR0RW0LF/set_the_sails/client/Set_the_sailsClient.java
@@ -1,10 +1,12 @@
 package com.ERR0RW0LF.set_the_sails.client;
 
+import com.ERR0RW0LF.set_the_sails.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
 
 public class Set_the_sailsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        ModItems.registerModItems();
     }
 }
--- src/main/java/com/ERR0RW0LF/set_the_sails/item/ModItems.java
+++ src/main/java/com/ERR0RW0LF/set_the_sails/item/ModItems.java
@@ -1,7 +1,9 @@
 package com.ERR0RW0LF.set_the_sails.item;
 
 import com.ERR0RW0LF.set_the_sails.Set_the_sails;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -10,12 +12,17 @@ public class ModItems {
 
     public static final Item PLANK = registerItems("plank", new Item(new Item.Settings()));
 
-    private static Item registerItems(String name, Item item){
+    public static Item registerItems(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(Set_the_sails.MOD_ID, name), item);
     }
 
-    private static void registerModItems(){
+    public static void registerModItems(){
         Set_the_sails.LOGGER.info("Registering Mod Items for " + Set_the_sails.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries ->{
+            entries.add(PLANK);
+        });
+
     }
 
 }
